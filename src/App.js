909c5ef36cc3320d9a5153cba96a2163efd35baa
Jason <kangja1996@gmail.com>
2021-09-08 00:29:59 -0400

    places item in the center

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Input from "./app/components/input";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       {/* todo list */}
       {/* done */}
       <Input />
