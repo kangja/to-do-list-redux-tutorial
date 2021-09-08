7e3c5b278bf8d22822cc575b4bf0db932f53cd5f
Jason <kangja1996@gmail.com>
2021-09-08 00:27:53 -0400

    progress

---
+++ src/app/components/input.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+function Input() {
+  const [input, setInput] = useState("");
+
+  const addTodo = () => {};
+
+  return (
+    <div className="input">
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={addTodo}>Add!</button>
+    </div>
+  );
+}
+
+export default Input;
